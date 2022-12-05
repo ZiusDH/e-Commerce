0a37afbdc66999f7212a6e15ebf881e8f7f98765
ZiusDH <dhiogomaquine@gmail.com>
2022-12-04 20:11:52 -0400

    Criando o CustomAppBar

--- lib/main.dart
+++ lib/main.dart
@@ -23,28 +23,43 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: Container(
-          color: Colors.blue,
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 10,
-          ),
-          child: Text(
-            'KAMAMMY',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: Color.fromARGB(255, 255, 168, 70),
-            ),
-          ),
-        ),
-        iconTheme: IconThemeData(color: Colors.blue),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.favorite))],
-      ),
+      appBar: CustomAppBar(),
       bottomNavigationBar: BottomAppBar(),
     );
   }
+}
+
+class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
+  const CustomAppBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      title: Container(
+        color: Colors.blue,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 10,
+        ),
+        child: Text(
+          'KAMAMMY',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 255, 168, 70),
+          ),
+        ),
+      ),
+      iconTheme: IconThemeData(color: Colors.blue),
+      actions: [IconButton(onPressed: () {}, icon: Icon(Icons.favorite))],
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => Size.fromHeight(50.0);
 }
